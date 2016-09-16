162439931aab92748179babea4afd55b8c718a46
Tracy Lee <tracy@wingman.events>
2016-09-16 04:06:16 -0400

    4th-run

---
+++ app/components/speaker-card.js
@@ -0,0 +1,41 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  speakers: [{
+        name: "Ben Lesh",
+        twitter: "benlesh",
+        picture: "ben-lesh.jpg"
+    },{
+        name: "Hans Larsen",
+        twitter: "hanslatwork",
+        picture: "hans-larsen.jpeg"
+   },{
+       name: "Aaron Frost",
+       twitter: "js_dev",
+       picture: "aaron-frost.jpg"
+  },{
+      name: "Gerard Sans",
+      twitter: "gerardsans",
+      picture: "gerard-sans.jpg"
+  },{
+      name: "Jeff Cross",
+      twitter: "jeffbcross",
+      picture: "jeff-cross.jpg"
+  },{
+      name: "Misko Hevery",
+      twitter: "mhevery",
+      picture: "misko-hevery.jpeg"
+  },{
+      name: "Joe Eames",
+      twitter: "josepheames",
+      picture: "joe-eames.jpeg"
+  },{
+      name: "Tracy Lee",
+      twitter: "ladyleet",
+      picture: "tracy-lee.jpg"
+  },{
+      name: "Victor Savkin",
+      twitter: "victorsavkin",
+      picture: "victor-savkin.jpg"
+  }]
+});
--- app/router.js
+++ app/router.js
@@ -7,8 +7,9 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('home');
+  this.route('home', {path: '/'});
   this.route('about');
+  this.route('speakers');
 });
 
 export default Router;
